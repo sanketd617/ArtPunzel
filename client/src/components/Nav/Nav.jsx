56c3e99f49160cb876f6412c7d93a24da92f2622
Sanket <2016bcs016@sggs.ac.in>
2019-09-27 01:42:30 +0530

    navbar done

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -5,8 +5,19 @@ import fontawesome from '@fortawesome/fontawesome';
 import {faChevronDown} from "@fortawesome/free-solid-svg-icons";
 import {connect} from "react-redux";
 import {toggleNavAction} from "../../actions/navActions";
+import {
+    faFacebook,
+    faGithub,
+    faGitlab,
+    faHackerrank,
+    faInstagram,
+    faLinkedin
+} from "@fortawesome/free-brands-svg-icons";
 
-fontawesome.library.add(faChevronDown);
+fontawesome.library.add(
+    faChevronDown, faFacebook, faInstagram,
+    faGithub, faLinkedin, faGitlab,
+    faHackerrank);
 
 class Nav extends React.Component {
     render() {
@@ -19,9 +30,45 @@ class Nav extends React.Component {
                     <FontAwesomeIcon icon="chevron-down"/>
                 </div>
                 <div className={ 'links ' + (this.props.isOpen ? 'open' : '') }>
-                    <a href="#">First</a>
-                    <a href="#">Second Link</a>
-                    <a href="#">Third option</a>
+                    <a href="#">Home</a>
+                    <a href="#">Singles</a>
+                    <a href="#">Couples</a>
+                    <a href="#">Black & White</a>
+                    <a href="#">Miscellaneous</a>
+
+                    <div className="social">
+                        <div className="title">
+                            Follow us on
+                        </div>
+                        <div className="social-btns">
+                            <FontAwesomeIcon icon={['fab', 'facebook']} size="2x" />
+                            <FontAwesomeIcon icon={['fab', 'instagram']} size="2x" />
+                        </div>
+                    </div>
+
+                    <div className="dev">
+                        <div className="title">Developer</div>
+                        <div className="dev-name">
+                            Sanket Dahegaonkar
+                        </div>
+                        <div className="social-links">
+                            <a href="#">
+                                <FontAwesomeIcon icon={['fab', 'linkedin']} /> sanketd617
+                            </a>
+                            <a href="#">
+                                <FontAwesomeIcon icon={['fab', 'github']} /> sanketd617
+                            </a>
+                            <a href="#">
+                                <FontAwesomeIcon icon={['fab', 'gitlab']} /> sanketd617
+                            </a>
+                            <a href="#">
+                                <FontAwesomeIcon icon={['fab', 'facebook']} /> sanketdgreat
+                            </a>
+                            <a href="#">
+                                <FontAwesomeIcon icon={['fab', 'hackerrank']} /> dsanket617
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
